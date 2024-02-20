92f791b114cd5200548781b7e741ad77f4c6b897
IgorTraspadini <igorchieppe@hotmail.com>
2024-02-20 10:02:27 -0300

    project-card component: css

--- React-portifolio-ICT/src/components/projects_card/project-card.jsx
+++ React-portifolio-ICT/src/components/projects_card/project-card.jsx
@@ -7,8 +7,8 @@ function ProjectCard(props) {
       <div className="card-body">
         <h5 className="card-title">{props.test}</h5>
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
+        <a href="#" className="card-link">Card link</a>
+        <a href="#" className="card-link">Another link</a>
       </div>
     </div>
   </>
