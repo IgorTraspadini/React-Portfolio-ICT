4eeed2437947751cdf459d3f172476ecdf658afc
IgorTraspadini <igorchieppe@hotmail.com>
2024-02-20 20:49:52 -0300

    redeploy

--- React-portifolio-ICT/vite.config.js
+++ React-portifolio-ICT/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // added to create a deply app
-  base: 'vite-portifolio-app',
+  base: 'React-portifolio-ICT',
   build: {
     outDir: 'build'
   },
