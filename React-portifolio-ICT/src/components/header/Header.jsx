251efe73efee000b3d813e2f2f40a467a9a08b88
IgorTraspadini <igorchieppe@hotmail.com>
2024-02-20 10:39:04 -0300

    project router: link adjusted on navbar

--- React-portifolio-ICT/src/components/header/Header.jsx
+++ React-portifolio-ICT/src/components/header/Header.jsx
@@ -1,29 +1,51 @@
-import './style.css'
+import './style.css';
+import { NavLink } from 'react-router-dom';
 
-function Header () {
+function Header() {
   return <>
     <nav className="navbar fixed-top navbar-expand-lg ">
-    <div className="container-fluid d-flex">
-      <h1 className="navbar-brand fw-bolder"><img src='./src/assets/logo.JPG' width='50px' alt='logo'></img> Igor Traspadini </h1>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav fw-bolder">
-          <li className="nav-item">
-            <a className="nav-link" href="#about" role="button" aria-label="about session">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#work" role="button" aria-label="work session">Projects</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#contact" role="button" aria-label="contact session">Contact</a>
-          </li>
-        </ul>
+      <div className="container-fluid d-flex">
+        <h1 className="navbar-brand fw-bolder"><img src='./src/assets/logo.JPG' width='50px' alt='logo'></img> Igor Traspadini </h1>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav fw-bolder">
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                end
+                className={({ isActive }) =>
+                  isActive ? 'nav-link active' : 'nav-link'
+                }
+              >Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/projects"
+                end
+                className={({ isActive }) =>
+                  isActive ? 'nav-link active' : 'nav-link'
+                }
+              >Projects
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/contact"
+                end
+                className={({ isActive }) =>
+                  isActive ? 'nav-link active' : 'nav-link'
+                }
+              >Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>  
+    </nav>
   </>
 }
 
