234e951f11e957c972be6c95c0278464fa417302
IgorTraspadini <igorchieppe@hotmail.com>
2024-02-20 10:14:56 -0300

    project-card component: link and icon

--- React-portifolio-ICT/src/components/projects_card/project-card.jsx
+++ React-portifolio-ICT/src/components/projects_card/project-card.jsx
@@ -7,8 +7,16 @@ function ProjectCard(props) {
       <div className="card-body">
         <h5 className="card-title">{props.test}</h5>
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="card-link">Card link</a>
-        <a href="#" className="card-link">Another link</a>
+        <a href="#" className="card-link">
+          <img className="mx-2 me-4" width="70px"
+            src="./src/assets/GitHub-Logo.png">
+          </img>
+        </a>
+        <a href="#" className="card-link">
+          <img className="mx-2 me-4" width="40px"
+            src="./src/assets/deploy.png">
+          </img>
+        </a>
       </div>
     </div>
   </>
