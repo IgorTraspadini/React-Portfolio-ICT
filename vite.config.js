dcb3109e2fec1c8a64322df3ae2d6678ed4dbb4b
IgorTraspadini <igorchieppe@hotmail.com>
2024-02-21 08:21:42 -0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+//import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // added to create a deply app
-  base: './React-Portifolio-ICT',
-  build: {
-    outDir: 'build'
-  },
+  //base: './React-Portifolio-ICT',
+//  build: {
+//    outDir: 'build'
+//  },
+  base: "https://igortraspadini.github.io/React-Portfolio-ICT",
   plugins: [react()],
 })
